2b4c884eb420a0ed1fc1e0ebf99b4aba947fa329
Demi Lune <anshikathakur596@gmail.com>
2024-09-10 11:56:34 +0530

    Update user.component.ts

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-user';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,15 +11,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar
+  // }
 
   onSelectedUser() {
     console.log('User selected: ', this.selectedUser.name);
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }

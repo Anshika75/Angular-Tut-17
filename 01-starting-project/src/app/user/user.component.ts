2185f7047d6528c199635fcab4e24fb89baa7ac7
Demi Lune <anshikathakur596@gmail.com>
2024-09-11 11:32:04 +0530

    Update user.component.ts

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -11,6 +11,7 @@ import { DUMMY_USERS } from '../dummy-user';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
   @Output() select = new EventEmitter<string>();

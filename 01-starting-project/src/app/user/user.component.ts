b852d9aa47574582f10b75803fa901472cd52697
Demi Lune <anshikathakur596@gmail.com>
2024-09-12 12:18:53 +0530

    accepting objects as inputs

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -11,9 +11,11 @@ import { DUMMY_USERS } from '../dummy-user';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name: string | undefined;
+  @Input({required: true}) user!:{
+    id: string;
+    name?: string | undefined;
+    avatar: string;
+  };
   // avatar = input<string>("");
   // name = input<string>("John Doe");
   // imagePath = computed(() => 'assets/users/' + this.avatar());
@@ -23,7 +25,7 @@ export class UserComponent {
   // select = output<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
   // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
@@ -33,7 +35,7 @@ export class UserComponent {
   // }
 
   onSelectedUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
     // console.log('User selected: ', this.selectedUser.name);
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // this.selectedUser.set(DUMMY_USERS[randomIndex]);

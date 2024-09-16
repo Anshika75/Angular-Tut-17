c0d6fae1b6571891b6bad6a844ab035d9e8aadf2
Demi Lune <anshikathakur596@gmail.com>
2024-09-16 10:07:46 +0530

    ng for and ngif

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-user';
@@ -6,16 +7,16 @@ import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1'
+  selectedUserId?: string;
 
-  get selectedUserName() {
-    return this.users.find(user => user.id === this.selectedUserId)?.name;
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId);
   }
 
   onSelectedUser(id: string) {

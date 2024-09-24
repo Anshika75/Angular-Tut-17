a74287ce5bef2dd6057c02fafd6355002afecf95
Demi Lune <anshikathakur596@gmail.com>
2024-09-24 10:29:39 +0530

    Update new-task.component.ts

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, inject, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type NewTaskData } from '../task/task.model';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,19 +11,28 @@ import { type NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+  @Input() userId!: string;
   @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  // @Output() add = new EventEmitter<NewTaskData>();
   enteredTitle = '';
   enteredSummary = '';
   enteredDueDate = '';
+
+  private tasksService = inject(TaskService)
+
   onCancel() {
     this.cancel.emit();
   }
   onSubmit() {
-    this.add.emit({
+    // this.add.emit({
+    //   title: this.enteredTitle,
+    //   summary: this.enteredSummary,
+    //   dueDate: this.enteredDueDate,
+    // });
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       dueDate: this.enteredDueDate,
-    });
+    }, this.userId);
   }
 }

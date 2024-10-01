87d4790901fc54b20fac972db43a15d4e9da6243
Demi Lune <anshikathakur596@gmail.com>
2024-10-01 12:54:15 +0530

    Update auth.directive.ts

--- 05-directives-project/src/app/auth/auth.directive.ts
+++ 05-directives-project/src/app/auth/auth.directive.ts
@@ -1,11 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, effect, inject, input } from '@angular/core';
+import { Permission } from './auth.model';
+import { AuthService } from './auth.service';
 
 @Directive({
   selector: '[appAuth]',
   standalone: true
 })
 export class AuthDirective {
-
-  constructor() { }
-
+  userType = input.required<Permission>({alias: 'appAuth'});
+  private authService = inject(AuthService)
+  constructor() { 
+    effect(() => {
+        if(this.authService.activePermission() === this.userType()){
+            console.log('User has permission');
+        }
+        else{
+            console.log('User does not have permission');
+        }
+  });
+}
 }

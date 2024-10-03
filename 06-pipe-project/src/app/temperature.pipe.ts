3cbe0b663bcfb467314d150a745fb3d08056ad19
Demi Lune <anshikathakur596@gmail.com>
2024-10-03 10:12:59 +0530

    Update temperature.pipe.ts

--- 06-pipe-project/src/app/temperature.pipe.ts
+++ 06-pipe-project/src/app/temperature.pipe.ts
@@ -1,4 +1,4 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform, output } from '@angular/core';
 
 @Pipe({
   name: 'temp', 
@@ -6,7 +6,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class TemperaturePipe implements PipeTransform {
-  transform(value: any, ...args: any[]): any {
-    return value + '°C';
+  transform(value:string | number, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah'){
+    let val: number;
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+        val = value;
+    }
+    let outputTemp: number;
+    if(inputType === 'cel' && outputType === 'fah'){
+        outputTemp = val * 9 / 5 + 32;   
+    } else if(inputType === 'fah' && outputType === 'cel'){
+        outputTemp = (val - 32) * 5 / 9;
+    } else {
+        outputTemp = val;
+    }
+
+    let symbol: '°F' | '°C';
+
+    if(!outputType){
+        symbol = inputType === 'cel' ? '°C' : '°F';
+    } else {
+        symbol = outputType === 'cel' ? '°C' : '°F';
+    }
+
+    return `${outputTemp.toFixed(2)} ${symbol}`;
   }
 }

03e4a3c0eecc404a31592111552a20dd6c05a744
Demi Lune <anshikathakur596@gmail.com>
2024-10-03 11:31:14 +0530

    Update tasks.service.ts

--- 07-service-project/src/app/tasks/tasks.service.ts
+++ 07-service-project/src/app/tasks/tasks.service.ts
@@ -1,23 +1,21 @@
 import { Injectable, signal } from '@angular/core';
+
 import { Task } from './task.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class TasksService {
-    tasks = signal<Task[]>([]);
-    
-    addTask(taskData: {
-        title: string,
-        description: string,
-    })  {
-        const newTask: Task = {
-            ...taskData, 
-            id: Math.random().toString(),
-            status: 'OPEN'
-        };
-        this.tasks.update((oldTasks) => [...oldTasks, newTask]);
-    }
+  private tasks = signal<Task[]>([]);
 
+  allTasks = this.tasks.asReadonly();
+
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN'
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
 }

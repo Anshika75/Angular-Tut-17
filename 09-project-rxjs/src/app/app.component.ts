5f5d7e4ecc859627d7c410f4a73ef257b01eea84
Demi Lune <anshikathakur596@gmail.com>
2024-10-07 11:26:15 +0530

    Update app.component.ts

--- 09-project-rxjs/src/app/app.component.ts
+++ 09-project-rxjs/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -11,6 +11,8 @@ export class AppComponent implements OnInit {
   private detroyRef = inject(DestroyRef);
   clickCount = signal(0);
   clickCount$ = toObservable(this.clickCount);
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$);
 
   constructor() {
     // effect(() => {

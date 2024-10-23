92e1dfb15128f7a1a7ea7649d105f7670d8e81d5
Demi Lune <anshikathakur596@gmail.com>
2024-10-23 11:46:21 +0530

    Update login.component.ts

--- 13-form-handling/src/app/auth/login/login.component.ts
+++ 13-form-handling/src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { stringify } from './../../../../node_modules/postcss/lib/postcss.d';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +11,23 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor(){
+    afterNextRender(() => {
+      const subscription = this.form()
+      .valueChanges?.pipe(debounceTime(500))
+      .subscribe({
+        next: (value) =>
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({email: value.email})
+          )
+        });
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    })
+  }
   onSubmit(formData: NgForm) {
     if (formData.invalid) {
       return;
